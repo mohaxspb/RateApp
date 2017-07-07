235d008dc631b1fd1ffcb2a294cd983fd2408273
mohax.spb <mohax.spb@gmail.com>
2017-07-08 01:55:33 +0300

    - initial commit

---
+++ app/src/main/java/ru/kuchanov/rateapp/sample/MainActivity.java
@@ -0,0 +1,28 @@
+package ru.kuchanov.rateapp.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.kuchanov.rate.PreRate;
+import ru.kuchanov.rateapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        PreRate.init(this, getString(R.string.feedback_email), getString(R.string.feedback_title)).showIfNeed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreRate.clearDialogIfOpen();
+    }
+}
